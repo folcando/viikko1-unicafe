286c18e235b6ce7ae80f924943f851b45bef19af
folcando <folcando@protonmail.com>
2025-06-08 19:52:44 +0300

    start 1.7

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
 import { useState } from 'react'
 
+const Statistics = ({good, neutral, bad}) => {
+  return (
+    <div>
+      <h1>statistics</h1>
+    </div>
+  )
+}
+
+const Button = () => {
+  return (
+    <div></div>
+  )
+}
+
 const App = () => {
-  // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
   return (
     <div>
-      code here
+      <h1>Give Feedback</h1>
+      
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
