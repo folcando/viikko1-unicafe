08fc2eae9e96d54fde3578572a282c3e0bade8ad
folcando <folcando@protonmail.com>
2025-06-09 13:40:03 +0300

    teht 1.7

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import { useState } from 'react'
 
 const Statistics = ({good, neutral, bad}) => {
+  const total = good + neutral + bad
+
   return (
     <div>
       <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {(good - bad) / total}</p>
+      <p>positive { (good / total) * 100 }%</p>
     </div>
   )
 }
 
-const Button = () => {
+const Button = ({onClick, text}) => {
   return (
-    <div></div>
+    <button onClick={onClick}>{text}</button>
   )
 }
 
@@ -19,10 +27,16 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const addGood = () => {
+    setGood(good + 1)
+  }
+
   return (
     <div>
       <h1>Give Feedback</h1>
-      
+      <Button onClick={addGood} text='good'/>
+      <Button onClick={() => setNeutral(neutral + 1)} text='neutral'/>
+      <Button onClick={() => setBad(bad + 1)} text='bad'/>
       <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
